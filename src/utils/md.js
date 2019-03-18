82ff6dc6b91f541ff8ba7775a6bb305991b612ac
li-xiaoqing <15010263998@163.com>
2019-03-18 15:35:29 +0800

    feat: a link target

--- src/utils/md.js
+++ src/utils/md.js
@@ -87,6 +87,19 @@ const installConfig = {
     katex: false,
     miip: true
 }
+// a link target
+const defaultRender = md.renderer.rules.link_open || function (tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options)
+}
+md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+    const aIndex = tokens[idx].attrIndex('target')
+    if (aIndex < 0) {
+        tokens[idx].attrPush(['target', '_blank'])
+    } else {
+        tokens[idx].attrs[aIndex][1] = '_blank'
+    }
+    return defaultRender(tokens, idx, options, env, self)
+}
 md.init = function (config) {
     const mergeConfig = Object.assign(installConfig, config)
     for (let key in mergeConfig) {
